5d8be81301e6ce63ea78b173a79c98ba3d633506
Amjad Khan <khan.amjad36@gmail.com>
2018-10-29 17:06:30 +0530

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Amjad Khan on 29/10/18.
+//  Copyright © 2018 Amjad Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
